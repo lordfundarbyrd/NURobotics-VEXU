66cc89e99c5da345d118ab0b517a282585315ba0
lordfundarbyrd <lordfundarbyrd@gmail.com>
2021-09-08 20:25:11 -0400

    Update intake.cpp

--- Tipping Point/15InchRobot/src/subsystemFiles/intake.cpp
+++ Tipping Point/15InchRobot/src/subsystemFiles/intake.cpp
@@ -6,10 +6,6 @@ void setIntake(int power) {
 }
 
 void setIntakeMotors() {
-    int intakePower = 0;
-    if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
-        intakePower = -127;
-    else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
-        intakePower = 127;
+    int intakePower = 127 * (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1) - controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2));
     setIntakePower(intakePower);
 }
